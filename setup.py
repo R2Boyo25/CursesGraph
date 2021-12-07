d0bdcaff8b68561e6d7188f4fe61bc9bf611f490
R2boyo25 <r2boyo25@gmail.com>
2021-12-07 17:04:29 -0600

    Structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='CursesGraph',
+    packages=find_packages(include=['CursesGraph']),
+    version='0.1.0',
+    description='Graphing library using Curses',
+    author='R2Boyo25',
+    license='LGPLv3',
+)
